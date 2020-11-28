cbd137ce7d5295521b3c8b36df29f05ea5ebf309
yesukay2 <yesukay2@gmail.com>
2020-11-28 21:03:54 +0000

    Started with project fronend

---
+++ dist/script.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function ifyes() {
+  if (document.getElementById('yes').checked) {
+    document.getElementById('ifyes').style.display = 'block';
+  } else {
+    document.getElementById('ifyes').style.display = 'none';
+  }
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+function ifyes(){
+    if (document.getElementById('yes').checked){
+        document.getElementById('ifyes').style.display='block';
+    }else{
+        document.getElementById('ifyes').style.display='none';
+    }
+}
