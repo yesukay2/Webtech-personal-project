eb00a92f11db74b375847c6f9fccf7390638db80
yesukay2 <yesukay2@gmail.com>
2020-12-11 14:47:11 +0000

    added tables for employee logs and horse logs

---
+++ employee-log-table.php
@@ -0,0 +1,137 @@
+
+<?php
+ $host = "localhost";
+ $username = "root";
+ $password = "";
+ $database_name = "saddle_rides";
+
+ //database connection 
+ $conn = new mysqli($host, $username, $password, $database_name);
+ 
+    if(isset($_GET['id_no'])) {
+        $id_no = $_GET['id_no'];
+        $query = "DELETE FROM employee_log WHERE id_no = '$id_no'";
+
+        $data = mysqli_query($conn, $query);
+
+        if ($data){
+            echo "<font color = 'green'>Record deleted!";
+        }else{
+            echo "<font color = 'red'>Failed to delete record!";
+        }
+    }
+?>
+
+<html>
+    <head>
+
+        <title>Saddle Rides Co. Ltd</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap"
+            rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.3.1/css/all.min.css">
+        <link rel="stylesheet" href="login.css">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="shortcut icon" href="img/favicon.ico">
+        <!-- Bootstrap 4.5 CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Style CSS -->
+        <link rel="stylesheet" href="css/style.css">
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&display=swap" rel="stylesheet">
+        <style type="text/css">
+            table{
+                border-collapse: collapse;
+                width: 100%;
+                color:  rgba(0,0,0,1);
+                font-family: monospace;
+                font-size: 15px;
+                text-align: left;
+            }
+
+            th{
+                background-color:  rgba(0,0,0,1);
+                color: white;
+            }
+
+            tr:nth-child(even){
+                background-color: #7F7A79;
+            }
+        </style>
+    </head>
+    <body>
+
+            	<!-- Top Bar -->
+            <div class="top-bar">
+                <div class="container">
+                    <div class="col-12 text-right">
+                        <p><a href="IT Support: +233558649054">IT Support: +233558649054</a></p>
+                    </div>
+                </div>
+            </div>
+            <!-- End Top Bar -->
+
+
+            <!-- Navigation -->
+            <nav class="navbar bg-dark navbar-dark navbar-expand-lg">
+                <div class="container">
+                    <a href="index.html" class="navbar-brand"><img src="img/logo.png" alt="Logo" title="Logo"></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarResponsive">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item"><a href="index.html" class="nav-link active">Home</a></li>
+                            <li class="nav-item"><a href="" class="nav-link">Sign Up</a></li>
+                            <li class="nav-item"><a href="" class="nav-link">Login</a></li>
+                            <li class="nav-item"><a href="" class="nav-link">Employee Log</a></li>
+                            <li class="nav-item"><a href="" class="nav-link">Horse Log</a></li>
+                        </ul>
+
+                    </div>
+                </div>
+            </nav>
+            <!-- End Navigation -->
+        <table>
+            <tr>
+                <th>Date</th>
+                <th>Name</th>
+                <th>ID NO</th>
+                <th>Vehicle</th>
+                <th>Vehicle Reg</th>
+                <th>Time Out</th>
+                <th>Destination</th>
+                <th>Time In</th>
+                <th>Operation</th>
+            </tr>
+            <?php 
+                $conn = mysqli_connect("localhost", "root", "", "saddle_rides");
+                $query = "SELECT date, employee_name, id_no, vehicle_option, vehicle_reg, time_out, dest_purp, time_in FROM employee_log";
+                $results = $conn->query($query);
+
+                if ($results->num_rows > 0){
+                    while ($row = $results->fetch_assoc()){
+                        echo "<tr><td>" . $row["date"] . "</td><td>" .  $row["employee_name"] . "</td><td>" . $row["id_no"] . "</td><td>" .  $row["vehicle_option"] . "</td><td>" .  $row["vehicle_reg"] . "</td><td>" .  $row["time_out"] ."</td><td>" .  $row["dest_purp"] . "</td><td>" .  $row["time_in"] ."</td><td> <a href = 'employee-log-table.php?id_no=$row[id_no]'>Delete</td></tr>";
+                    }
+                }else{
+                    echo "No Results";
+                }
+                $conn->close();
+            ?>
+        </table>
+
+        <!-- Script Source Files -->
+
+        <!-- jQuery -->
+        <script src="js/jquery-3.5.1.min.js"></script>
+        <!-- Bootstrap 4.5 JS -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Popper JS -->
+        <script src="js/popper.min.js"></script>
+        <!-- Font Awesome -->
+        <script src="js/all.min.js"></script>
+
+        <!-- End Script Source Files -->
+    </body>
+</html>
